2824664af16f9403f15c3af9b3a5960a7e2116f7
Elvir Nuriahmetov <elvir.nf@gmail.com>
2017-08-28 16:20:23 +0300

    add events

--- contracts/Iq300Token.sol
+++ contracts/Iq300Token.sol
@@ -16,6 +16,7 @@ contract Iq300Token is StandardToken, Active {
        bool isPaid;
        uint createdAt;
     }
+
     struct Participant {
       uint userId;
       address addr;
@@ -26,7 +27,21 @@ contract Iq300Token is StandardToken, Active {
 
     Participant public mainCustomer;
     Participant public mainExecutor;
+
     mapping (address => Participant) public subExecutors;
+
+    event ParticipantJoined(
+        address indexed _addr,
+        uint _itaskId,
+        uint _userId
+    );
+
+    event TaskPaid(
+        address indexed _addr,
+        uint _itaskId,
+        uint _userId,
+        uint256 _amount
+    );
 
     function Iq300Token(string _name, string _symbol, uint initialSupply, uint _projectId) {
         name = _name;
@@ -44,15 +59,32 @@ contract Iq300Token is StandardToken, Active {
         mainExecutor = Participant({userId: userId, addr: _addr, paidAmount: 0, reservedAmount: 0});
     }
 
-    function addParticipant(address _addr, uint _userId, uint _taskId, uint256 _amount) onlyOwner onlyActive {
+    function addParticipant(
+        address _addr,
+        uint _userId,
+        uint _taskId,
+        uint256 _amount
+    ) onlyOwner onlyActive returns (bool) {
         require(balances[this] - _amount - reserved >= 0);
-        if (subExecutors[_addr].userId == 0) {
-            subExecutors[_addr] = Participant({userId: _userId, addr: _addr, paidAmount: 0, reservedAmount: 0});
+        var executor = subExecutors[_addr];
+        if (executor.userId == 0) {
+            subExecutors[_addr] = Participant({
+                userId: _userId,
+                addr: _addr,
+                paidAmount: 0,
+                reservedAmount: 0
+            });
         }
-        subExecutors[_addr].reservedAmount += _amount;
+        executor.reservedAmount += _amount;
         reserved += _amount;
-        require(subExecutors[_addr].payments[_taskId].createdAt == 0);
-        subExecutors[_addr].payments[_taskId] = TaskPayment({amount: _amount, isPaid: false, createdAt: now});
+        require(executor.payments[_taskId].createdAt == 0);
+        executor.payments[_taskId] = TaskPayment({
+            amount: _amount,
+            isPaid: false,
+            createdAt: now
+        });
+        ParticipantJoined(_addr, _userId, _taskId);
+        return true;
     }
 
     function getParticipantPaidAmount(address _addr) returns (uint256) {
@@ -63,33 +95,45 @@ contract Iq300Token is StandardToken, Active {
         return subExecutors[_addr].reservedAmount;
     }
 
-    function payForTask(address _addr, uint taskId) onlyOwner onlyActive {
-        require(subExecutors[_addr].userId != 0);
+    function payForTask(address _addr, uint taskId) onlyOwner onlyActive returns (bool) {
         var participant = subExecutors[_addr];
-        require(participant.payments[taskId].createdAt != 0);
-        require(participant.payments[taskId].isPaid == false);
-        var payment = subExecutors[_addr].payments[taskId];
+        require(participant.userId != 0);
+        var payment = participant.payments[taskId];
+        require(payment.createdAt != 0);
+        require(payment.isPaid == false);
         if (payment.amount == 0) {
-            return;
+            return false;
         }
         participant.reservedAmount -= payment.amount;
         participant.paidAmount += payment.amount;
         payment.isPaid = true;
         reserved -= payment.amount;
-        transferFrom(this, _addr, 100);
+        if (transferFromOwner(_addr, payment.amount)) {
+            TaskPaid(_addr, taskId, participant.userId, payment.amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function transferFromOwner(address _addr, uint256 _amount) onlyOwner onlyActive returns (bool) {
+        require(balances[this] >= _amount);
+        balances[this] = balances[this].sub(_amount);
+        balances[_addr] = balances[_addr].add(_amount);
+        return true;
     }
 
      function close() onlyOwner onlyActive  {
-         require (reserved == 0);
          deactivate();
          if (balances[this] == 0) {
              return;
          }
          if (mainExecutor.userId == 0) {
-             transfer(owner, balances[this]);
+             balances[owner] = balances[this];
          }
          else {
-             transfer(mainExecutor.addr, balances[this]);
+             balances[mainExecutor.addr] = balances[this];
          }
+         balances[this] = 0;
      }
 }
